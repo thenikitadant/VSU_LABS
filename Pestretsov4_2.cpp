99aafeade63c4c25f4e74caaf6f98a722294229d
thenikitadant <73257125+thenikitadant@users.noreply.github.com>
2020-10-22 17:30:26 +0300

    Add files via upload

---
+++ Pestretsov4_2.cpp
@@ -0,0 +1,39 @@
+﻿// Введите 3 числа. Если они могут быть длинами строн равнобедренного остроугольного треугольника, вычислите его площадь.
+// Выведите длины сторон и площадь в порядке возрастания значений.
+
+// Пестрецов Н. - 1 курс, 4 группа
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	double a, b, c, S, p;
+	// length = 4, т.к. сортируются 4 значения (a, b, c, S)
+	const int length = 4;
+	cout << "enter a: ";
+	cin >> a;
+	cout << "enter b: ";
+	cin >> b;
+	cout << "enter c: ";
+	cin >> c;
+	if ((a + b > c) && (a + c > b) && (b + c > a))
+		if ((a == b) || (a == c) || (b == c))
+			if ((pow(a, 2) + pow(b, 2) > pow(c, 2)) || (pow(a, 2) + pow(c, 2) > pow(b, 2)) || (pow(b, 2) + pow(c, 2) > pow(a, 2)))
+			{
+				p = (a + b + c) / 2;
+				S = sqrt(p * (p - a) * (p - b) * (p - c));
+				cout << "S = " << S << endl;
+				double array[length] = { a, b, c, S };
+				sort(array, array + length);
+				for (int i = 0; i < length; ++i)
+				cout << array[i] << " ";
+			}
+			// если не остроугольный!
+			else cout << "!Triangel isn't acute-angled";
+		// если не равнобедренный!
+		else cout << "!Triangle isn't isosceles";
+	// если не может являться треугольником!
+	else cout << "!Wrong sides of a triangle";
+	return 0;	
+}
