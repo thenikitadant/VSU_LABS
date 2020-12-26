c26b573bc2bbcd79bd6461fe2973f9e68e8c325e
thenikitadant <73257125+thenikitadant@users.noreply.github.com>
2020-12-26 05:21:11 +0300

    Add files via upload

---
+++ Pestretsov4_5.cpp
@@ -0,0 +1,95 @@
+﻿// #4 lab.
+// Даны действительные числа x и ε. Вычислить с точностью ε. Проверить x<>0.
+
+// Пестрецов Н. - 1 курс, 4 группа
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int znam, fact, stepen;
+	int N, kolvo, i, j;
+	double a, x, E, S1 = 0, S2 = 0, S3 = 0;
+	
+	cout << "enter N: ";
+	cin >> N;
+	cout << "enter E: ";
+	cin >> E;
+	cout << "enter x (x <> 0): ";
+	cin >> x;
+	a = x;
+	
+
+	while (pow(E, x) > S1)											// общее условие.
+	{
+		for (i = 1; i < N; i++)										// нахождение 1-ой суммы.
+		{
+			S1 += a;
+			znam = 1; stepen = 1;
+			fact = znam;
+			j = i;
+
+			while (j != 0)											// нахождение факториала (знаметеля) в слагаемом N;
+			{
+				fact *= j; j--;
+			}
+
+			a = pow(x, stepen) / fact;
+			znam++; stepen++;
+			cout << "a = " << a << endl;
+		}
+
+		
+		a = x;
+		if (abs(a) > abs(E)) {										// нахождение 2-ой суммы.
+			i = 1;
+			while (abs(a) > abs(E))
+			{
+				S2 += a;
+				znam = 1; stepen = 1;
+				fact = znam;
+				j = i;
+
+				while (j != 0)
+				{
+					fact *= j; j--;
+				}
+
+				a = pow(x, stepen) / fact;
+				znam++; stepen++; i++;
+			}
+		}
+
+
+		a = x;
+		if (abs(a) > abs(0.1 * E)) {								// нахождение 3-ой суммы.
+			i = 1; kolvo = 1;
+			while (abs(a) > abs(0.1 * E))
+			{
+				S3 += a;
+				znam = 1; stepen = 1;
+				fact = znam;
+				j = i;
+
+				while (j != 0)
+				{
+					fact *= j; j--;
+				}
+
+				a = pow(x, stepen) / fact;
+				znam++; stepen++; i++; kolvo++;
+			}
+		}
+	}
+
+	cout << endl << "***RESULT***" << endl;
+	cout << "1'st summ = " << S1 << endl;
+	cout << "2'nd summ = " << S2 << endl;
+	cout << "3'rd summ = " << S3 << endl;
+	cout << "kol-vo = " << kolvo << endl;
+
+
+	return 0;
+	system("pause");
+}
---
+++ Pestretsov4_6.cpp
@@ -0,0 +1,48 @@
+﻿// #6 lab.
+// Дано вещественное число R и массив размера N. Найти два элемента массива, сумма которых наименее близка к данному числу.
+
+// Пестрецов Н. - 1 курс, 4 группа
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	int n, i, j, numi, numj;
+	float* a, bub, bliz, max, R;
+
+	cout << "enter n: ";
+	cin >> n;
+	cout << "enter R: ";
+	cin >> R;
+
+	a = new float[n];
+	for (i = 0; i < n; i++)
+		cin >> a[i];
+
+	bub = a[1] * a[2];
+	if (bub > R)
+		max = bub - R;
+	if (bub <= R)
+		max = R - bub;
+	bliz = max;
+
+	for (i = 0; i < n - 1; i++)
+		for (j = i + 1; j < n; j++) {
+			bub = a[i] + a[j];
+			if (bub >= R)
+				max = bub - R;
+			if (bub <= R)
+				max = R - bub;
+			if (max > bliz) {
+				bliz = max; numi = i; numj = j;
+			}
+		}
+
+	cout << endl << "***RESULT***" << endl;
+	cout << "bliz = " << bliz << endl;
+	cout << "1'st element = " << numi + 1 << endl;
+	cout << "2'nd element = " << numj + 1 << endl;
+
+	return 0;
+}
